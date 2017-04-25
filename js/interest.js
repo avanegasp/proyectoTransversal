dc13ff177095e16d915762597eccd1a658674ae9
Angie Vanegas <arco527@gmail.com>
2017-04-24 22:48:34 -0500

    Interest animation JS

--- js/interest.js
+++ js/interest.js
@@ -1,4 +1,36 @@
 
+// FUNCION ANIMACIÓN
+
+var nave = new Image();
+var fondo = new Image();
+nave.src="img/marciano2.png";
+fondo.src="img/puntos.jpg";
+var posx=1;
+var posy=100;
+var dir = 5; // pixeles para desplazarse
+
+function dibuja(){
+  setInterval(animacion, 50);
+}
+
+function animacion(){
+  var anima = document.getElementById("milienzo");
+  var contexto = anima.getContext('2d');
+  contexto.drawImage(fondo, 0, 0, 400, 200); // weidth=400; heigt=300;
+  contexto.font="bold 18px Arial";
+  contexto.fillStyle="#FFFFFF";
+  contexto.fillText("CALCULADORAS DE COLOMBIA", 50, 40); //posx = 50; posy = 40;
+  if(posx>=330 || posx<=0){
+    dir=dir*-1;
+  }
+  posx=posx+dir;
+  contexto.drawImage(nave,posx,posy,80,100);
+}
+
+
+
+// Función para interest
+
   function valorInicial(){
 
     var valor=document.getElementById("valorInicial").value;
